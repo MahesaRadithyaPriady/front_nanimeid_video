683dca0d89de990557c24a507a0a899b62846a36
MahesaRadithyaPriady <Mahesaradithyapriady@gmail.com>
2025-10-19 18:39:25 +0700

    add go backend url fix single page

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://103.103.23.250:8080'
+export const API_BASE = import.meta.env.VITE_API_BASE || ''
 export const api = (path = '') => `${API_BASE}${path}`
