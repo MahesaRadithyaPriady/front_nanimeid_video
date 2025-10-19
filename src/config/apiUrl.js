a54085c20314fa468052af8b1bea351529c4b927
MahesaRadithyaPriady <Mahesaradithyapriady@gmail.com>
2025-10-19 18:57:48 +0700

    https fix issuee

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || ''
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://media.nanimeid.xyz/'
 export const api = (path = '') => `${API_BASE}${path}`
